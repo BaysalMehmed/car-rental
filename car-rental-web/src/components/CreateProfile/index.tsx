ad4c84db19cbb1cbced811e6f4cb7fed32ec7b19
Baysal Mehmed <baysalmehmed1@gmail.com>
2022-08-17 18:49:39 +0100

    CSS create profile

--- car-rental-web/src/components/CreateProfile/index.tsx
+++ car-rental-web/src/components/CreateProfile/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import iProfile from "../model/iProfile"
 import { createProfile, getProfile } from "../service/ProfileService"
+import { MdOutlineErrorOutline } from 'react-icons/md';
 
 
 interface iFieldValidation {
@@ -25,34 +26,33 @@ export default function CreateProfile() {
         return (
             <>
                 <div>
-                    <label htmlFor="firstName">First Name: </label>
-                    <input type="text" id="firtName" onChange={(value => setFirstName(validate(value.target.value, value.target.value.length >= 5, "First Name must contain more than 4 characters")))} />
-                    <b className="error-text"> {firstName.error && firstName.error}</b>
+                    <input className="input-div double-field" placeholder="First Name" type="text" id="firtName" onChange={(value => setFirstName(validate(value.target.value, value.target.value.length >= 5, "First Name must contain more than 4 characters")))} />
+                    <MdOutlineErrorOutline className="error-red error-icon"/>
+                    <input className="input-div double-field" placeholder="Surname" type="text" id="surname" onChange={(value => setSurname(validate(value.target.value, value.target.value.length >= 5, "Surname must contain more than 4 characters")))} />
                 </div>
-                <div><label htmlFor="surname">Surname: </label>
-                    <input type="text" id="surname" onChange={(value => setSurname(validate(value.target.value, value.target.value.length >= 5, "Surname must contain more than 4 characters")))} />
-                    <b className="error-text"> {surname.error && surname.error}</b>
-                    </div>
-                <div><label htmlFor="email">Email: </label>
-                    <input type="text" id="email" onChange={(value => setEmail(validate(value.target.value, value.target.value.includes("@"), "Must be a valid email")))} />
-                    <b className="error-text"> {email.error && email.error}</b>
-                    </div>
-                <div><label htmlFor="phoneNumber">Phone Number: </label>
-                    <input type="text" id="phoneNumber" onChange={(value => setPhoneNumber(validate(value.target.value, true, "Must be a valid email")))} />
-                    <b className="error-text"> {phoneNumber.error && phoneNumber.error}</b>
-                    </div>
+                <div>
+                    <input className="input-div" placeholder="Email" type="text" id="email" onChange={(value => setEmail(validate(value.target.value, value.target.value.includes("@"), "Must be a valid email")))} />
+
+                </div>
+                <div>
+                    <input className="input-div" placeholder="Phone Number" type="text" id="phoneNumber" onChange={(value => setPhoneNumber(validate(value.target.value, true, "Must be a valid email")))} />
+
+                </div>
             </>
 
         )
     }
 
-    return (<>
-        {getForm()}
-        <div><button disabled={firstName.error !== "" || surname.error !== "" || email.error !== "" || phoneNumber.error !== ""}
-            onClick={() => createProfile({ firstName: firstName.value, surname: surname.value, email: email.value, phoneNumber: phoneNumber.value })
-                .then(data => console.log(data))}>
-            Submit
-        </button>
+    return (
+        <div className="create-profile-container">
+            {getForm()}
+            <div>
+                <button className="submit-button" disabled={firstName.error !== "" || surname.error !== "" || email.error !== "" || phoneNumber.error !== ""}
+                    onClick={() => createProfile({ firstName: firstName.value, surname: surname.value, email: email.value, phoneNumber: phoneNumber.value })
+                        .then(data => console.log(data))}>
+                    Submit
+                </button>
+            </div>
         </div>
-    </>)
+    )
 }
